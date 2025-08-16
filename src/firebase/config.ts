3d52dfe4bc2f580dc0b41cf048dcaa153c5ed38e
IsraelPolice <dvirbareket4@gmail.com>
2025-08-16 19:59:17 +0300

    Fix connectFirestoreEmulator import

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,8 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
-import { connectAuthEmulator, connectFirestoreEmulator } from 'firebase/auth';
+import { connectAuthEmulator } from 'firebase/auth';
+import { connectFirestoreEmulator } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
